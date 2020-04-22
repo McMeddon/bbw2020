8cc51f72c21a2abf0c4c45c9486b9446cd327d99
CR92dev <chrizroland2@web.de>
2020-04-22 20:11:20 +0200

    streams.html added prototyp

---
+++ js/twitch.js
@@ -0,0 +1,89 @@
+function updateStreams() {
+    //todo
+    //var streams = ["monstercat", "moondye7", "handiofiblood"];
+    //for (let i = 0; i < streams.length; i++) {
+    //   getTwitchChannelStatus(streams[i]);
+    //}
+}
+
+function getTwitchChannelStatus(channel) {
+    var objUser;
+    var objChannel;
+    getTwitchUser(channel, function (x) {
+        if (x === "error") {
+            console.log(x);
+        }
+        else {
+            objUser = JSON.parse(x);
+        }
+    })
+    getStatus(objUser.data[0].id, function (x) {
+        if (x === "error") {
+            console.log(x);
+        }
+        else {
+            objChannel = JSON.parse(x);
+            var live;
+            var viewers;
+            if (typeof objChannel.data[0] !== 'undefined') {
+                live = objChannel.data[0].type;
+                viewers = objChannel.data[0].viewer_count;
+            }
+            var element = document.getElementById(channel + "_icon");
+            if (live === 'live') {
+                element.classList.add("online");
+                element.innerHTML = viewers;
+            } else {
+                element.classList.remove("online");
+                element.innerHTML = "";
+            }
+        }
+    })
+}
+
+function getStatus(userid, callback) {
+    var request = new XMLHttpRequest();
+
+    request.addEventListener('load', function (event) {
+        if (request.status >= 200 && request.status < 300) {
+            //console.log(request.responseText);
+            callback(request.responseText);
+        } else {
+            //console.warn(request.statusText, request.responseText);
+            callback("error");
+        }
+    });
+
+    request.open("GET", "https://api.twitch.tv/helix/streams?user_id=" + userid, false);
+    request.setRequestHeader("Client-ID", "tuchqw8ynn12oipzun9slynagyciqm");
+    request.send(null);
+}
+
+function getTwitchUser(user, callback) {
+    var request = new XMLHttpRequest();
+
+    request.addEventListener('load', function (event) {
+        if (request.status >= 200 && request.status < 300) {
+            //console.log(request.responseText);
+            callback(request.responseText);
+        } else {
+            //console.warn(request.statusText, request.responseText);
+            callback("error");
+        }
+    });
+
+    request.open("GET", "https://api.twitch.tv/helix/users?login=" + user, false);
+    request.setRequestHeader("Client-ID", "tuchqw8ynn12oipzun9slynagyciqm");
+    request.send(null);
+}
+
+function hold() {
+
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            callback(request.response);
+        }
+    }
+
+}
