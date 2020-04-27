ed2035e3a88ad9055dac9563404588d878b38de3
PatricJacob <pajacob44@gmail.com>
2020-04-27 13:06:13 +0200

    minor chanches js

--- js/steam.js
+++ js/steam.js
@@ -4,7 +4,7 @@ function loadBBGroupData() {
     xhr.open("GET", "https://steamcommunity.com/groups/BladeBrothers/memberslistxml/?xml=1", true);
 
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             console.log (xhr.responseText);
             //document.querySelector(".listxmp").innerHTML = xhr.responseText;
         }
